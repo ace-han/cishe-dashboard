35ca41fa4df22fd581597bff4cfd4bb611e3acdb
Ace <ace.jl.han@gmail.com>
2020-10-13 23:44:39 +0800

    add parseMoment vue filter

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,1 +1,14 @@
-export { parseTime } from '@/utils'
+import moment from 'moment'
+
+export const parseMoment = (m: string | moment.Moment, format?: string): string => {
+  let result = ''
+  if (!m) {
+    return result
+  }
+  if (format) {
+    result = moment(m).format(format)
+  } else {
+    result = moment(m).format()
+  }
+  return result
+}
